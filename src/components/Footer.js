d0e16259eead620b119e4cb427637cecc031c6c4
Nathan Stepper <nstepper@uw.edu>
2023-06-18 18:51:13 -0700

    trying to get app to function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Footer = () => (
     <footer>
-        <a href="https://github.com/yourusername">GitHub</a>
-        <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
-        <a href="https://stackoverflow.com/users/youruserid">Stack Overflow</a>
+        <a href="https://github.com/nstepper" target="_blank" rel="noopener noreferrer">GitHub</a>
     </footer>
 );
 
