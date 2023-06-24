1d47adb31a6114e8b7e8b9046ad1862aaf42fecb
Nathan Stepper <nstepper@uw.edu>
2023-06-23 23:39:48 -0700

    last touches i could get

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import * as react from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  react.render(<App />);
+  const linkElement = react.screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
